70d12c36fafa0ebe38c09ab6c3431af6c76ff19b
dropelega <pdredondo@gmail.com>
2015-10-21 20:19:29 +0200

    Variables
    Inicialización de variables

--- libreria.c
+++ libreria.c
@@ -1,1 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+
+tline* tokenize(char* str);
+
+struct tcommand {
+	char *filename;
+	int argc;
+	char **argv;
+}
+
+typedef struct tcommand tcommand;
+
+struct tline {
+	int ncommands;
+	tcommand *commands;
+	char *redirect_input;
+	char *redirect_output;
+	char *redirect_error;
+}
+
+typedef struct tline tline;
+
+tline* tokenize(char* str){
+
+	if (str[0]=='\0'){
+
+		fprintf(stderr,"No se ha añadido nada");
+
+	}else{
+
+
+		int i=0;
+		while (str[i]!='\0'){
+
+		}
+
+	}
+
+}

dadecd18b591a16dd3dab36f3d48b86bc0d5856a
valli22 <davidvacasmiguel@hotmail.com>
2016-05-19 19:39:35 +0200

    VA: memoria practica 1

--- VA/Practica1VA/cascadeClassifier.py
+++ VA/Practica1VA/cascadeClassifier.py
@@ -3,16 +3,15 @@ import cv2
 
 class CascadeClassifier:
     def __init__(self,path):
-        self.path = path
+        self.cascada = cv2.CascadeClassifier(path)
     def procesamiento(self,imgs,minSize):
         '''
         :param imgs: array de imagenes a procesar
         :return: array de imagenes de entrada con un rectangular pintado alrededor de cada coche detectado y de
-        un tamaño minimo de 100*100 px
+        un tamaño minimo proporcionado por el segundo valor de entrada
         '''
         for img in imgs:
-            cascada = cv2.CascadeClassifier(self.path)
-            rectangulos = cascada.detectMultiScale(img, minNeighbors=2, scaleFactor=1.4, minSize=minSize)
+            rectangulos = self.cascada.detectMultiScale(img, minNeighbors=2, scaleFactor=1.4, minSize=minSize)
             for rect in rectangulos:
                 x, y, w, h = rect
                 cv2.rectangle(img, (x, y + h), (x + w, y), (0, 255, 0))

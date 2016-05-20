f61ef3a6abb2a1d1a638c2ca5759d78d0c3809b1
dropelega <pdredondo@gmail.com>
2016-05-20 12:04:03 +0200

    Fin practica 2

--- VA/Practica2VA/main.py
+++ VA/Practica2VA/main.py
@@ -9,7 +9,9 @@ training = []
 C = []
 E = []
 cont = 0
-path = 'C:\Users\pdred\Desktop\VA\Practica2/training_ocr/training_ocr'
+
+# Creamos el array de caracteristicas y el array de etiquetas a partir de las imagenes de entrenamiento proporcionadas
+path = raw_input('Introduzca la direccion donde se encuentran las imagenes para entrenar:')
 for (dirpath, dirname, filename) in os.walk(path):
     for file in filename:
         pathFile = path +'/'+file
@@ -26,11 +28,14 @@ for (dirpath, dirname, filename) in os.walk(path):
         else:
             E.append(ord(label))
 
+# Inicializamos LDA y reducimos las dimensiones de C
 sklearn_lda = LDA()
 E = np.asarray(E, dtype=np.float32)
 sklearn_lda.fit(C,E)
 CR = sklearn_lda.transform(C)
 CR = np.asarray(CR, dtype=np.float32)
+
+# Inicializamos el clasificador y lo entrenamos
 
 # EM de precision
 # em = cv2.EM()
@@ -50,14 +55,19 @@ testing = []
 path = raw_input('Introduzca el path donde se encuentren los archivos para realizar las pruebas:')
 nombre = path.split('\\')[-1]
 fileopen = open(nombre+'.txt','w')
+
+pathHaar = raw_input('Introduzca la direccion del clasificador de matriculas:')
 for (dirpath, dirname, filename) in os.walk(path):
+    # Inicializamos un clasificador con uno ya entrenado para detectar matriculas
+
+
     for p,file in enumerate(filename):
         pathFile = path +'/'+file
         I = cv2.imread(pathFile,0)
         testing.append(I)
 
-        # Deteccion frontal del coche
-        cascada = cv2.CascadeClassifier('C:\Users\pdred\Desktop\VA\Practica1\haar\haar/matriculas.xml' )
+        cascada = cv2.CascadeClassifier(pathHaar)
+        # Deteccion de las matriculas de la imagen
         rectangulos = cascada.detectMultiScale(I, minNeighbors=2, scaleFactor=1.4)
 
         for rect in rectangulos:
@@ -68,16 +78,20 @@ for (dirpath, dirname, filename) in os.walk(path):
             matricula = ""
             for des in caracteres:
 
+                # Reducimos las dimensiones del vector de caracteristicas de la imagen
                 newDes = sklearn_lda.transform([des])
 
+                # Buscamos el descriptor mas parecido
                 # retvals,results = normal.predict(np.asarray(newDes,np.float32))
 
                 # retvals, results = em.predict(np.asarray(newDes,np.float32))
 
                 ret,results,neighbours,dist = knn.find_nearest(np.asarray(newDes,np.float32),k=5)
 
+                # En caso de que reconozca un valor ESP su valor sera 10000 y por lo tanto lo ignoramos
                 if results[0][0]!=10000:
                     matricula+=chr(results[0][0])
+
             string = 'Nombre imagen: '+file+' Coordenadas centro matricula x: '+str(w/2)+' y: '+str(h/2)+' Matricula: '+matricula+' Largo matricula / 2: '+str(w/2)+'\n'
             fileopen.write(string)
 fileopen.close()
